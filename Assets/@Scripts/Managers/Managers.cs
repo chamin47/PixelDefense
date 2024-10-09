05f21cb9e085572c253f8ef0fb8ca836029a0af0
chamin47 <152050575+chamin47@users.noreply.github.com>
2024-10-10 04:05:44 +0900

    [feat] Managers 추가

--- Assets/@Scripts/Managers/Managers.cs
+++ Assets/@Scripts/Managers/Managers.cs
@@ -1,11 +1,50 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class Managers : MonoBehaviour
-{   
-    void Start()
-    {
-        
-    } 
+{
+	static Managers s_instance;
+	static Managers Instance { get { Init(); return s_instance; } }
+
+	#region Contents
+	GameManager _game = new GameManager();
+	ObjectManager _object = new ObjectManager();
+	public static GameManager Game { get { return Instance?._game; } }
+	public static ObjectManager Object { get { return Instance?._object; } }
+	#endregion
+
+	#region Core
+	DataManager _data = new DataManager();
+	PoolManager _pool = new PoolManager();
+	ResourceManager _resource = new ResourceManager();
+	SceneManagerEx _scene = new SceneManagerEx();
+	SoundManager _sound = new SoundManager();
+	UIManager _ui = new UIManager();
+	public static DataManager Data { get { return Instance?._data; } }
+	public static PoolManager Pool { get { return Instance?._pool; } }
+	public static ResourceManager ResourceManager { get { return Instance?._resource; } }
+	public static SceneManagerEx Scene { get { return Instance?._scene; } }
+	public static SoundManager SoundManager { get { return Instance?._sound; } }
+	public static UIManager UI { get { return Instance?._ui; } }
+	#endregion
+
+	public static void Init()
+	{
+		if (s_instance != null)
+			return;
+
+		GameObject go = GameObject.Find("@Managers");
+		if (go == null)
+		{
+			go = new GameObject() { name = "@Managers" };
+			go.AddComponent<Managers>();
+		}
+
+		DontDestroyOnLoad(go);
+		s_instance = go.GetComponent<Managers>();
+	}
+
+	public static void Clear()
+	{
+
+	}
 }
---
+++ Assets/@Scripts/Utils/Define.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Define
+{
+	public enum Scene
+	{
+		Unknown,
+		TitleScene,
+		LobbyScene,
+		GameScene
+	}
+
+	public enum Sound
+	{
+		Bgm,
+		Effect,
+		Max,
+	}
+}
